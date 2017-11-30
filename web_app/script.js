ec913d5f724872f39aab3745f713749dd2d04d79
Pierre Maurier <pierre.maurier@gadz.org>
2017-11-30 11:50:55 +0100

    Add server side google maps geolocation + db

--- web_app/script.js
+++ web_app/script.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
@@ -12,22 +14,15 @@ app.set('port', (process.env.PORT || 5000));
 // set up Mongoose connection
 const mongoose = require('mongoose');
 var mongoDB = process.env.MONGODB_URI;
-mongoose.connect(mongoDB, {
-  useMongoClient: true
-});
+mongoose.Promise = global.Promise
+mongoose.connect(mongoDB);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var LocationSchema = new mongoose.Schema({
-  location: String,
   date: Date,
   latitude: Number,
-  longitude: Number,
-  altitude: Number,
-  accuracy: Number,
-  altitudeAccuracy: Number,
-  heading: Number,
-  speed: Number
+  longitude: Number
 });
 
 var Location = mongoose.model('Location', LocationSchema);
@@ -36,15 +31,26 @@ var NamedLocationSchema = new mongoose.Schema({
   name: String,
   date: Date,
   latitude: Number,
-  longitude: Number,
-  altitude: Number,
-  accuracy: Number,
-  altitudeAccuracy: Number,
-  heading: Number,
-  speed: Number
+  longitude: Number
 });
 
 var NamedLocation = mongoose.model('NamedLocation', NamedLocationSchema);
+
+var googleMapsClient = require('@google/maps').createClient({
+  key: process.env.GOOGLE_MAPS_API
+});
+
+app.get("/getplan", (req, res) => {
+  NamedLocation.find().sort({date: 1}).exec(function(err, data) {
+    res.json(data);
+  });
+});
+
+app.get("/getpath", (req, res) => {
+  Location.find({}).sort({date: 1}).exec(function(err, data) {
+    res.json(data);
+  });
+});
 
 app.get("/addcity", (req, res) => {
   res.sendFile(path.join(__dirname, 'private/index.html'));
@@ -52,18 +58,46 @@ app.get("/addcity", (req, res) => {
 
 app.post("/addcity", (req, res) => {
   var myData;
-  if (req.body.name) {
-    myData = new Location(req.body);
-  } else {
-    myData = new NamedLocation(req.body);
-  }
-  myData.save()
-    .then(item => {
-      res.send("Ville ajoutée");
-    })
-    .catch(err => {
-      res.status(400).send("Impossible de sauvegarder dans la BDD");
+  // Geocode an address.
+  googleMapsClient.geocode({
+    address: req.body.name
+  }, function(err, response) {
+    if (!err) {
+      myData = new NamedLocation({
+        name: req.body.name,
+        date: new Date(req.body.date),
+        latitude: response.json.results[0].geometry.location.lat,
+        longitude: response.json.results[0].geometry.location.lng
+      });
+      myData.save()
+        .then(item => {
+          res.send("Ville ajoutée");
+        })
+        .catch(err => {
+          res.status(400).send("Impossible de sauvegarder dans la BDD");
+        });
+      res.sendFile(path.join(__dirname, 'private/index.html'));
+    }
+  });
+});
+
+app.post("/addlocation", (req, res) => {
+  var myData;
+  console.log(req.body);
+  // for (let i = 0; i < req.body.length; i++) {
+    myData = new Location({
+      date: req.body.timestamp,
+      latitude: req.body.latitude,
+      longitude: req.body.longitude
     });
+    myData.save()
+      .then(item => {
+        res.send("Point ajouté");
+      })
+      .catch(err => {
+        res.status(400).send("Impossible de sauvegarder dans la BDD");
+      });
+  // }
 });
 
 app.use("/", (req, res) => {
